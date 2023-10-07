b47fe693781182bf30817c1160c82d825602822f
jorgeguedess <jorge_guedes00@hotmail.com>
2023-10-07 15:46:49 -0300

    feat: error validation and yup translation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -16,13 +16,20 @@ export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
   } catch (error) {
     const yupError = error as yup.ValidationError;
-    return res.json({
-      errors: {
-        default: yupError.message,
-      },
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach((error) => {
+      if (!error.path) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors,
     });
   }
   console.log(validatedData);
